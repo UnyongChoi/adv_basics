c2ba671675763c53d848a21696029b8e06c7917b
UnyongChoi <unyongchoi@gmail.com>
2023-12-07 08:26:30 +0900

    stateful widget으로 보강

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
-import 'package:adv_basics/start_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 84, 4, 222),
-              Color.fromARGB(255, 86, 58, 152),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const StartScreen(),
-      ),
-    ),
-  ));
+  runApp(const Quiz());
 }
