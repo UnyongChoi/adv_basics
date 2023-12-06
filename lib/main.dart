c47d67b6ed5a6fd8d285f8e3fdba6bbb775a98c6
UnyongChoi <unyongchoi@gmail.com>
2023-12-07 07:55:07 +0900

    initial push

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 84, 4, 222),
+              Color.fromARGB(255, 86, 58, 152),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const StartScreen(),
+      ),
+    ),
+  ));
+}
