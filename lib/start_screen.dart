10d5a289ebd820ad41b1fdb1c32806e8a0197a04
UnyongChoi <unyongchoi@gmail.com>
2023-12-07 09:25:26 +0900

    화면 전환 함수 전달 코드 추가

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuizScreen, {super.key});
+
+  final void Function() startQuizScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +26,7 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuizScreen,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
